25c56ca730ac98e7a6d6fbe4caf268dbcdec5ccd
Ivan L <ivanlatiy@gmail.com>
2026-07-28 13:10:41 +0300

    remove dead code

--- src/data/index.ts
+++
@@ -1,4 +0,0 @@
-export type { Category, SelectField, Service, Master } from "./types";
-export { categories } from "./categories";
-export { services } from "./services";
-export { masters } from "./masters";
